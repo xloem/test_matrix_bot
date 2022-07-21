1eeebfb4711205770d367c0b56ab4cd3973d38fe
John Doe <john.doe@example.com>
2022-07-21 10:22:27 -0400

    add exception handler and command to raise one

--- test_matrix_bot.py
+++ test_matrix_bot.py
@@ -34,6 +34,10 @@ class ExampleBot(MatrixBotAPI):
         pdb_handler = MRegexHandler("open%pdb", self.pdb_callback)
         self.add_handler(pdb_handler)
 
+        # Add a regex handler for raising an exception
+        exc_handler = MRegexHandler("raise%exception", self.exc_callback)
+        self.add_handler(exc_handler)
+
         # Add a regex handler waiting for the echo command
         echo_handler = MCommandHandler("echo", self.echo_callback)
         self.add_handler(echo_handler)
@@ -43,18 +47,35 @@ class ExampleBot(MatrixBotAPI):
         self.add_handler(dieroll_handler)
 
     def handle_message(self, room, event):
-        sender = event['sender']
-        padding = ' ' * len(sender)
-        if 'body' in event['content']:
-            body = event['content']['body']
-        else:
-            body = repr(event['content'])
-        lines = body.split('\n')
-        logger.info(f'{room.name} {sender}: {lines[0]}')
-        for extra_line in lines[1:]:
-            logger.info(f'{room.name} {padding}: {extra_line}')
-
-        super().handle_message(room, event)
+        try:
+            sender = event['sender']
+            padding = ' ' * len(sender)
+            if 'm.relates_to' in event['content']:
+                if 'm.in_reply_to' in event['content']['m.relates_to']:
+                    reply_id = event['content']['m.relates_to']['m.in_reply_to']['event_id']
+                else:
+                    reply_id = event['content']['m.relates_to']['event_id']
+            else:
+                reply_id = None
+            if event['type'] == 'm.room.message':
+                message = event['content']['body']
+            else:
+                message = f"{event['type']}: {repr(event['content'])}"
+            lines = message.split('\n')
+            logger.info(f'{room.name} {sender}: {lines[0]}')
+            for extra_line in lines[1:]:
+                logger.info(f'{room.name} {padding}: {extra_line}')
+    
+            super().handle_message(room, event)
+        except Exception as exception:
+            import traceback
+            exc_str = traceback.format_exc()#eption(exception, value=None, tb=None)
+            logger.error(f'{room.name} {repr(event)}')
+            for line in exc_str.split('\n'):
+                if line.endswith('\n'): # for using format_exception instead of format_exc
+                    line = line[:-1]
+                logger.error(line)
+                room.send_text(line)
 
     def start_polling(self):
         # Start polling
@@ -67,6 +88,10 @@ class ExampleBot(MatrixBotAPI):
         room.send_text("I'm opening a PDB session to look at my code. You can look at this too, at https://github.com/xloem/test_matrix_bot . TODO: put commit hash here")
         import pdb; pdb.set_trace()
         room.send_text("The PDB session has closed.")
+
+    def exc_callback(self, room, event):
+        room.send_text('Are you sure? This is really scary ... Here we go, I\'ll raise an exception.')
+        raise Exception(f"{event['sender']} asked me to raise this but I'm worried about it.")
 
     def hi_callback(self, room, event):
         # Somebody said hi, let's say Hi back
